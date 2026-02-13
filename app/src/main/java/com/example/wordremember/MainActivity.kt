42955da07e31f6e28287f13f2b48907cb4718d96
Ali <72344723+alisabourii@users.noreply.github.com>
2026-02-13 12:44:48 +0300

    add function for screenshots

--- app/src/main/java/com/example/wordremember/MainActivity.kt
+++ app/src/main/java/com/example/wordremember/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.wordremember
 
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.text.format.DateFormat
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.RecyclerView
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.util.Date
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnAdd:Button
@@ -36,7 +44,58 @@ class MainActivity : AppCompatActivity() {
         btnSave = findViewById(R.id.btnSave)
         btnSave.setOnClickListener {
             Log.e("MSG","LAW LAW LAW")
+
+            takeScreenshot()
+
         }
     }
+
+
+    fun takeScreenshot() {
+        val rootView = window.decorView.rootView
+        val bitmap = Bitmap.createBitmap(
+            rootView.width,
+            rootView.height,
+            Bitmap.Config.ARGB_8888
+        )
+
+        val canvas = Canvas(bitmap)
+        rootView.draw(canvas)
+
+        saveBitmap(bitmap)
+    }
+    fun saveBitmap(bitmap: Bitmap) {
+        val filename = "screenshot_${System.currentTimeMillis()}.png"
+
+        val fos: OutputStream?
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val resolver = contentResolver
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/Screenshots")
+            }
+
+            val imageUri = resolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+
+            fos = imageUri?.let { resolver.openOutputStream(it) }
+        } else {
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
+        }
+    }
+
+
+
 
 }
