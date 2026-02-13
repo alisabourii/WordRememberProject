e213c59254bfefffb616fb40915950ca38319a8b
Ali <72344723+alisabourii@users.noreply.github.com>
2026-02-13 12:23:58 +0300

    add basic codes

--- app/src/main/java/com/example/wordremember/MainActivity.kt
+++ app/src/main/java/com/example/wordremember/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.wordremember
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnAdd:Button
+    private lateinit var btnSave:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scrollArea)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnAdd = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+            Log.e("MSG","HEY HEY HEY")
+        }
+
+        btnSave = findViewById(R.id.btnSave)
+        btnSave.setOnClickListener {
+            Log.e("MSG","LAW LAW LAW")
+        }
     }
+
 }
